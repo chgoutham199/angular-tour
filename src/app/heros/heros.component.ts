c8edadbc550e0a51b5908eb8a2b9bf07d0105bde
chgoutham199 <chgoutham199@gmail.com>
2024-08-16 15:22:52 +0530

    angular demo

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent {
+     heros=HEROES;
+     selectedHero?:Hero;
+     onSelect(hero :Hero) : void{
+       this.selectedHero=hero;
+      //  console.log(hero);
+     }
+}
